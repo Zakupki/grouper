2fc7e4954c4e9bdde3ee013b1783e96377cf5235
orange22 <dmitriy.bozhok@gmail.com>
2014-02-25 09:01:58 +0200

    12

---
+++ app/modules/admin/controllers/Public_Controller.php
@@ -0,0 +1,36 @@
+<?
+require_once 'modules/admin/controllers/BaseAdmin_Controller.php';
+require_once 'modules/admin/models/Publics.php';
+
+Class Public_Controller Extends BaseAdmin_Controller {
+		public $registry;
+		public $error;
+		private $take=50;
+		private $page=1;
+		
+		public function __construct($registry){
+			parent::__construct($registry);
+			$this->registry=$registry;
+			$this->view = new View($this->registry);
+			$this->Publics = new Publics;
+		}
+
+        function indexAction() {
+        	$this->publiccount=$this->Publics->getPublicCount();
+			if(tools::int($this->registry->rewrites[1])>0)
+			$this->page=tools::int($this->registry->rewrites[1]);
+        	$this->Pageing = new Pageing('admin/public', $this->take, $this->publiccount, $this->page);
+			$this->pager=$this->Pageing->GetAdminHTML();
+        	$this->publiclist=$this->Publics->getPublicList($this->Pageing->getStart(), $this->take);
+			$this->content =$this->view->AddView('public', $this);
+			$this->view->renderLayout('admin', $this);
+		}
+		function userinnerAction() {
+			$this->userinner=$this->Publics->getPublicInner($this->registry->rewrites[1]);
+			$this->content =$this->view->AddView('userinner', $this);
+			$this->view->renderLayout('admin', $this);
+		}
+}
+
+
+?>
