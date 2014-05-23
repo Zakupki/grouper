0e433518ada9a23ead0d656c74101e01579c7727
orange22 <dmitriy.bozhok@gmail.com>
2014-05-23 13:58:30 +0300

    12

--- app/modules/report/views/moregroups.php
+++ app/modules/report/views/moregroups.php
@@ -21,8 +21,6 @@
 										<?}?>
 									</a>
 								</div>
-
-								<a class="address i <?=$socicons[$club['socialid']];?>"><?=$club['subject'];?></a>
                                 <a class="address i"><?=$club['subject'];?><img src="<?=$club['socialimage'];?>"/></a>
 							</div>
 							<a class="hl stats" href="/group/<?=$club['id'];?>/">
